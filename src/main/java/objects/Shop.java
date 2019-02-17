dce145099659cb3b560bcaaa82302d19610cd5ff
Eskil <apan1451@gmail.com>
2019-02-17 17:40:56 +0100

    Created shop class

--- src/main/java/objects/Shop.java
+++ src/main/java/objects/Shop.java
@@ -2,10 +2,38 @@ package objects;
 
 import java.util.ArrayList;
 
-//TODO make this class
 public class Shop {
+    private String name;
+    private int chunkLocation;
+    private ArrayList<String> stock;
+    private ArrayList<String> questReqs;
+
+    public Shop (String name, int chunkLocation, ArrayList<String> stock) {
+        this.name = name;
+        this.chunkLocation = chunkLocation;
+        this.stock = stock;
+    }
+
+    public Shop (String name, int chunkLocation, ArrayList<String> stock, ArrayList<String> questReqs) {
+        this.name = name;
+        this.chunkLocation = chunkLocation;
+        this.stock = stock;
+        this.questReqs = questReqs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChunkLocation() {
+        return chunkLocation;
+    }
+
     public ArrayList<String> getStock() {
-        //TODO
-        return null;
+        return stock;
+    }
+
+    public ArrayList<String> getQuestReqs() {
+        return questReqs;
     }
 }
