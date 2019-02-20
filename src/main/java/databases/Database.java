653ebe4c5fa7092c8c35dc2bc3d549b274c6c721
Eskil <apan1451@gmail.com>
2019-02-20 01:00:17 +0100

    Created Database class

---
+++ src/main/java/databases/Database.java
@@ -0,0 +1,21 @@
+package databases;
+
+import objects.NamedThing;
+
+import java.util.HashMap;
+
+public class Database<T extends NamedThing> {
+
+	private HashMap<String, T> database = new HashMap<String, T>();
+
+	public T getElement(String name) {
+		return database.get(name);
+	}
+
+	public boolean contains(T element){ return database.containsValue(element); }
+
+	public boolean contains(String key){ return database.containsKey(key); }
+
+	public void registerElement(T obj){ if(!database.containsKey(obj.getName())) database.put(obj.getName(), obj); }
+
+}
