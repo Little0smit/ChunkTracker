1b52cc1f12b810676cffded9c9519fe588e20c14
Apan145 <apan1451@gmail.com>
2019-02-24 22:10:16 +0100

    Made the main method do more stuff

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,12 @@
 import JSON.DataParser;
+import objects.Player;
 
 public class main {
     public static void main(String[] args) {
         DataParser.parse();
+        Player player = new Player(0);
+
+        player.skillsToNextChunk();
     }
 }
 
