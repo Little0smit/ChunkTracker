b29bd0ddb0838d10c255e0bed64d86af34d30bee
Apan145 <apan1451@gmail.com>
2019-02-27 23:27:11 +0100

    Added test methods

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,9 +4,10 @@ import objects.Player;
 public class main {
     public static void main(String[] args) {
         DataParser.parse();
-        Player player = new Player(0);
+        Player player = new Player(1);
 
         player.skillsToNextChunk();
+        player.BiSItems();
     }
 }
 
