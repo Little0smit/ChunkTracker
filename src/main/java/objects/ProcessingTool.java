d2b0eb644335581126af31556f27b02851a323d9
Apan145 <apan1451@gmail.com>
2019-02-27 23:31:19 +0100

    Restructured class

--- src/main/java/objects/ProcessingTool.java
+++ src/main/java/objects/ProcessingTool.java
@@ -3,25 +3,19 @@ package objects;
 import java.util.ArrayList;
 
 public class ProcessingTool extends NamedThing {
-    private ArrayList<Process> processes;
-    private int chunkLocation;
-    private String processToolName;
+    private ArrayList<Process> processes = new ArrayList<Process>();
+    private String processToolName = "";
+    private ArrayList<String> processesNames = new ArrayList<String>();
 
-    public ProcessingTool(ArrayList<Process> processes, int chunkLocation, String processToolName) {
+    /*public ProcessingTool(ArrayList<Process> processes, int chunkLocation, String processToolName) {
         this.processes = processes;
         this.chunkLocation = chunkLocation;
         this.processToolName = processToolName;
-    }
+    }*/
 
-    public ArrayList<Process> getProcesses() {
-        return processes;
-    }
+    public ArrayList<Process> getProcesses() { return processes; }
 
-    public int getChunkLocation() {
-        return chunkLocation;
-    }
+    public String getName() { return processToolName; }
 
-    public String getProcessName() {
-        return processToolName;
-    }
+    public ArrayList<String> getProcessesNames() { return processesNames; }
 }
