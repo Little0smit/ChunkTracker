b29c9b34ee8d86dce79f39ad1ae6a977921f4a06
Apan145 <apan1451@gmail.com>
2019-03-03 23:30:04 +0100

    Removed constructor and reworked getProcesses method

--- src/main/java/objects/ProcessingTool.java
+++ src/main/java/objects/ProcessingTool.java
@@ -1,21 +1,23 @@
 package objects;
 
+import constants.Constant;
+
 import java.util.ArrayList;
 
 public class ProcessingTool extends NamedThing {
-    private ArrayList<Process> processes = new ArrayList<Process>();
+    //TODO: Rename to SkillingLocation
     private String processToolName = "";
     private ArrayList<String> processesNames = new ArrayList<String>();
-
-    /*public ProcessingTool(ArrayList<Process> processes, int chunkLocation, String processToolName) {
-        this.processes = processes;
-        this.chunkLocation = chunkLocation;
-        this.processToolName = processToolName;
-    }*/
-
-    public ArrayList<Process> getProcesses() { return processes; }
 
     public String getName() { return processToolName; }
 
     public ArrayList<String> getProcessesNames() { return processesNames; }
+
+    public ArrayList<Process> getProcesses(){
+        ArrayList<Process> al = new ArrayList<Process>();
+        for (String string : processesNames){
+            al.add(Constant.PROCESS_DATABASE.getElement(string));
+        }
+        return al;
+    }
 }
