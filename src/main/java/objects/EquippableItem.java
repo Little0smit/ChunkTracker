c1208ce2793371ce25878b2e01fe403cb4272f65
Apan145 <apan1451@gmail.com>
2019-03-04 21:39:07 +0100

    Changed inheritance and reworked getBiSNumber method

--- src/main/java/objects/EquippableItem.java
+++ src/main/java/objects/EquippableItem.java
@@ -3,16 +3,12 @@ package objects;
 import constants.EquipmentSlot;
 import constants.WeaponType;
 
-public class EquippableItem extends NamedThing{
-	private String name;
+public class EquippableItem extends Item{
 	private int[] offensiveStats, //stab, slash, crush, magic, ranged
 			defensiveStats, //stab, slash, crush, magic, ranged
 			otherStats; //strength, ranged strength, magic strength, prayer, attack speed
 	private WeaponType type;
 	private EquipmentSlot slot;
-
-	@Override
-	public String getName() { return name; }
 
 	public int[] getOffensiveStats() { return offensiveStats; }
 
@@ -24,10 +20,23 @@ public class EquippableItem extends NamedThing{
 
 	public EquipmentSlot getSlot() { return slot; }
 
-	public int getBiSNumber() throws Exception{
-		if (type == WeaponType.Magic) throw new Exception();
-		return (Math.max(Math.max(offensiveStats[0], offensiveStats[1]), offensiveStats[2]) +
-				((type == WeaponType.Melee) ? otherStats[0] : otherStats[1])) /
-				otherStats[4];
+	public int getBiSNumber(){
+		switch (type){
+			case Magic:
+				return (offensiveStats[3] +
+						otherStats[2]) /
+						otherStats[4];
+			case Melee:
+				return (Math.max(Math.max(offensiveStats[0], offensiveStats[1]), offensiveStats[2]) +
+						otherStats[0]) /
+						otherStats[4];
+			case Ranged:
+				return (offensiveStats[4] +
+						otherStats[1]) /
+						otherStats[4];
+		}
+
+		//This shouldn't ever be triggered
+		return 0;
 	}
 }
