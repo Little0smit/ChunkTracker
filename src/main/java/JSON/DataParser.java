fd9b37b15ce2b694bec9c365776a5df4f09b3c41
Apan145 <apan1451@gmail.com>
2019-03-05 01:02:24 +0100

    Added parsing for the dummy files

--- src/main/java/JSON/DataParser.java
+++ src/main/java/JSON/DataParser.java
@@ -11,16 +11,26 @@ import java.io.*;
 
 public class DataParser {
 
-	public static void parse (){
+	public static void parse (boolean dummy){
 		try {
-			parseFile("src/main/resources/Mobs.json", Constant.MOB_DATABASE, Mob[].class);
-			parseFile("src/main/resources/Shops.json", Constant.SHOP_DATABASE, Shop[].class);
-			parseFile("src/main/resources/Processes.json", Constant.PROCESS_DATABASE, Process[].class);
-			parseFile("src/main/resources/Quests.json", Constant.QUEST_DATABASE, Quest[].class);
-			parseFile("src/main/resources/Chunks.json", Constant.CHUNK_DATABASE, Chunk[].class);
-			parseFile("src/main/resources/ProcessingTools.json", Constant.PROCESSING_TOOL_DATABASE, ProcessingTool[].class);
-			parseFile("src/main/resources/EquippableItems.json", Constant.EQUIPPABLE_ITEM_DATABASE, EquippableItem[].class);
-
+			if (dummy) {
+				System.out.println("bajs");
+				parseFile("src/main/resources/DummyMobs.json", Constant.MOB_DATABASE, Mob[].class);
+				parseFile("src/main/resources/DummyShops.json", Constant.SHOP_DATABASE, Shop[].class);
+				parseFile("src/main/resources/DummyProcesses.json", Constant.PROCESS_DATABASE, Process[].class);
+				parseFile("src/main/resources/DummyQuests.json", Constant.QUEST_DATABASE, Quest[].class);
+				parseFile("src/main/resources/DummyChunks.json", Constant.CHUNK_DATABASE, Chunk[].class);
+				parseFile("src/main/resources/DummySkillingLocations.json", Constant.SKILLING_LOCATION_DATABASE, SkillingLocation[].class);
+				parseFile("src/main/resources/DummyEquippableItems.json", Constant.EQUIPPABLE_ITEM_DATABASE, EquippableItem[].class);
+			} else {
+				parseFile("src/main/resources/Mobs.json", Constant.MOB_DATABASE, Mob[].class);
+				parseFile("src/main/resources/Shops.json", Constant.SHOP_DATABASE, Shop[].class);
+				parseFile("src/main/resources/Processes.json", Constant.PROCESS_DATABASE, Process[].class);
+				parseFile("src/main/resources/Quests.json", Constant.QUEST_DATABASE, Quest[].class);
+				parseFile("src/main/resources/Chunks.json", Constant.CHUNK_DATABASE, Chunk[].class);
+				parseFile("src/main/resources/SkillingLocations.json", Constant.SKILLING_LOCATION_DATABASE, SkillingLocation[].class);
+				parseFile("src/main/resources/EquippableItems.json", Constant.EQUIPPABLE_ITEM_DATABASE, EquippableItem[].class);
+			}
 			for (Chunk chunk : Constant.CHUNK_DATABASE.getAllElements()){
 				chunk.addItemsToDB();
 			}
