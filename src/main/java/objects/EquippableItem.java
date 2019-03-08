7d4fddf1a826e7abad9846015c2610a0cc5748b4
Apan145 <apan1451@gmail.com>
2019-03-08 12:10:31 +0100

    Improved best in slot calculation

--- src/main/java/objects/EquippableItem.java
+++ src/main/java/objects/EquippableItem.java
@@ -6,7 +6,7 @@ import constants.WeaponType;
 public class EquippableItem extends Item{
 	private int[] offensiveStats, //stab, slash, crush, magic, ranged
 			defensiveStats, //stab, slash, crush, magic, ranged
-			otherStats; //strength, ranged strength, magic strength, prayer, attack speed
+			otherStats; //strength, ranged strength, magic damage, prayer, attack speed
 	private WeaponType type;
 	private EquipmentSlot slot;
 
@@ -21,22 +21,26 @@ public class EquippableItem extends Item{
 	public EquipmentSlot getSlot() { return slot; }
 
 	public int getBiSNumber(){
-		switch (type){
-			case Magic:
-				return (offensiveStats[3] +
-						otherStats[2]) /
-						otherStats[4];
-			case Melee:
-				return (Math.max(Math.max(offensiveStats[0], offensiveStats[1]), offensiveStats[2]) +
-						otherStats[0]) /
-						otherStats[4];
-			case Ranged:
-				return (offensiveStats[4] +
-						otherStats[1]) /
-						otherStats[4];
+		if (slot == EquipmentSlot.MainHand || slot == EquipmentSlot.OffHand || slot == EquipmentSlot.Ammo){
+			switch (type){
+				case Magic:
+					return offensiveStats[3];
+				case Melee:
+					return (Math.max(Math.max(offensiveStats[0], offensiveStats[1]), offensiveStats[2]) +
+							otherStats[0]) /
+							otherStats[4];
+				case Ranged:
+					return (offensiveStats[4] +
+							otherStats[1]) /
+							otherStats[4];
+			}
+		} else {
+			return otherStats[0] + Math.max(Math.max(defensiveStats[0], defensiveStats[1]), defensiveStats[2])/1000;
 		}
 
 		//This shouldn't ever be triggered
 		return 0;
 	}
+
+	public int getPrayerNumber(){ return otherStats[3]; }
 }
